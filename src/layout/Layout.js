03d6d1788ca04286b0848f9dc714b888635c931d
DSM4 <fatec-dsm4@academico.intra>
2025-04-30 20:01:51 -0300

    feat: correção do esqueleto da tela home

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Header from "@/components/Header/Header";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import Footer from "@/components/Footer/Footer";
-import styles from "@/layout/Layout.module.css"
+import styles from "@/layout/Layout.module.css";
 
 export default function Layout({ children }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -19,17 +19,17 @@ export default function Layout({ children }) {
         <Header toggleSidebar={toggleSidebar} />
       </header>
 
-      <div className={`${styles.container} ${sidebarOpen ? styles.sidebarOpen : ""}`}>
+      <div
+        className={`${styles.container} ${
+          sidebarOpen ? styles.sidebarOpen : ""
+        }`}
+      >
         <aside className={styles.sidebar}>
           <Sidebar />
         </aside>
 
-        <main className={styles.main}>
-          {children}
-        </main>
+        <main className={styles.main}>{children}</main>
       </div>
-
-      
     </>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,51 +1,16 @@
-import MilkProduction from "@/components/Home/MilkProduction/MilkProduction";
-import TopBuffaloesChart from "@/components/Home/TopBuffaloesChart/TopBuffaloesChart";
-import Layout from "@/layout/Layout.js";
 import styles from "@/styles/Home.module.css";
+import Layout from "@/layout/Layout.js";
 
 export default function Home() {
   return (
     <>
-      <div className={styles.container}>
-        <h1 className={styles.message}>Olá, João Barreto!</h1>
-        <p className={styles.subtitle}>
-          Bem-vindo ao dashboard da sua fazenda de búfalos. Aqui está o resumo
-          de hoje.
-        </p>
-        <div className={styles.cardsContainer}>
-          <div className={styles.card}>
-            <h3>Total de búfalos</h3>
-            <p className={styles.cardValue}>247</p>
-            <p className={styles.cardInfo}>Machos: 186 (25% do rebanho)</p>
-            <p className={styles.cardInfo}>Fêmeas: 186 (75% do rebanho)</p>
-          </div>
-
-          <div className={styles.card}>
-            <h3>Machos</h3>
-            <p className={styles.cardValue}>186</p>
-            <p className={styles.cardInfo}>25% do rebanho</p>
-          </div>
-
-          <div className={styles.card}>
-            <h3>Fêmeas</h3>
-            <p className={styles.cardValue}>186</p>
-            <p className={styles.cardInfo}>75% do rebanho</p>
-          </div>
-
-          <div className={styles.card}>
-            <h3>Funcionários</h3>
-            <p className={styles.cardValue}>15</p>
-            <p className={styles.cardInfo}>Equipe completa</p>
-          </div>
-        </div>
+      <div className={styles.parent}>
+        <div className={styles.div1}>Div 1</div>
+        <div className={styles.div2}>Div 2</div>
+        <div className={styles.div3}>Div 3</div>
+        <div className={styles.div4}>Div 4</div>
+        
       </div>
-
-      <div className={styles.sectionRow}>
-        <MilkProduction />
-        <TopBuffaloesChart />
-      </div>
-
-     
     </>
   );
 }
