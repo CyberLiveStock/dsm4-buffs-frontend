b17ee8bd69b2aed03bb6ea73ebddb701b2262801
JoaoKuzinor <joaopedrokuzinorlima@hotmail.com>
2025-05-14 22:05:56 -0300

    Feat: Desenvolvimento dos filtros da tabela de lactação

--- src/pages/lactacao.js
+++ src/pages/lactacao.js
@@ -1,10 +1,45 @@
 import Layout from "@/layout/Layout.js";
 import styles from "@/styles/Lactacao.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function Lactacao() {
   return (
     <div className={styles.container}>
       <div className={styles.containerTable}>
+        <div class={styles.filtrosContainer}>
+          <div className={styles.filtroGroup}>
+            <label className={styles.filtroLabel}>Buscar por Tag</label>
+              
+            <input
+              type="text"
+              class={styles.filtroInput}
+              placeholder="Digite a tag"
+            />
+          </div>
+
+          <div className={styles.filtroGroup}>
+            <label className={styles.filtroLabel}>Status de lactação</label>
+            <select class={styles.filtroSelect}>
+              <option>Status de lactação</option>
+              <option>Ativa</option>
+              <option>Em queda</option>
+              <option>Secando</option>
+            </select>
+          </div>
+
+          <div className={styles.filtroGroup}>
+            <label className={styles.filtroLabel}>Localização</label>
+            <select className={styles.filtroSelect}>
+              <option>Piquete</option>
+              <option>Piquete 1</option>
+              <option>Piquete 2</option>
+            </select>
+          </div>
+
+          <button class={styles.filtroBtn}>Limpar filtros</button>
+        </div>
+
         <div>
           <table className={styles.tabela}>
             <thead className={styles.thead}>
@@ -55,7 +90,7 @@ export default function Lactacao() {
               </tr>
 
               <tr className={styles.trPar}>
-                <td className={styles.td}>BF003</td>s
+                <td className={styles.td}>BF003</td>
                 <td className={styles.td}>Beleza</td>
                 <td className={styles.td}>7.60 L</td>
                 <td className={styles.td}>52.50 L</td>
